a9862642a1e4c4956b19d99775d24b7b28a8a7cd
sobolevaea <liziaka@mail.ru>
2025-06-30 11:40:18 +0300

    add new console test

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,6 +1,25 @@
+import fs from 'fs'
+import path from 'path'
+import { fileURLToPath } from 'url'
+import { expect, test } from '@jest/globals'
+
+const __filename = fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
 import reverse from '../src/index.js'
+
+const getFixturePath = (filename) => path.join(__dirname, '..', '__fixtures__', filename);
+const readFixtureFile = (filename) => fs.readFileSync(getFixturePath(filename), 'utf-8');
+
 
 test('reverse', () => {
   expect(reverse('hello')).toEqual('olleh')
   expect(reverse('')).toEqual('')
+})
+
+test('reverse with log text', () => {
+  const text = readFixtureFile('before.txt').trim()
+  const expected = readFixtureFile('result.txt').trim()
+
+  expect(reverse(text)).toEqual(expected)
 })
